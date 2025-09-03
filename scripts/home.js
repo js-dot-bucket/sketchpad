4737b34a281b6d48ecd2139fc96025b2da836ddf
shubham sharma <shubhamsh3099@gmail.com>
2025-09-03 07:43:27 +0000

    Create a sketch pad
    Turn box color blue on mouse click or drag and lightgrey on hover

--- scripts/home.js
+++ scripts/home.js
@@ -5,3 +5,23 @@ for (let i=0; i<256; i++){
   container.appendChild(box);
 }
 
+let mousedown = false;
+
+document.body.addEventListener("mouseup", (e) => {
+  mousedown = false;
+  e.preventDefault();
+});
+
+["mousemove", "mousedown", "mouseup", "dragstart"].forEach((event) => { container.addEventListener(event, (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains("box")) {
+      if (e.type === "mousedown") {
+        mousedown = true;
+        e.target.style.backgroundColor = "blue";
+      }
+      if (e.type === "mousemove" && mousedown) {
+        e.target.style.backgroundColor = "blue";
+      }
+    }
+  });
+});
